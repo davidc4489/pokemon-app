33abb5ff636139c9e3d21788da3adbd858e81029
davidc4489 <davidc4489@gmail.com>
2024-07-23 11:01:29 +0300

    pok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
-@Component({
-  selector: 'app-root',
+@Component({              // Permet de construire un composant web
+  selector: 'app-root',   // Permet de donner un nom au composant afin de l'identifier
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  // Permet de definir le contenu html du composant
+  templateUrl: 'app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'pokemon-app';
+
+export class AppComponent implements OnInit {
+  pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon | undefined ;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList) ;
+  }
+
+  selectPokemon(event : MouseEvent) {
+    const index : number = Number((event.target as HTMLInputElement).value) ;
+    if(index < 12 && index >= 0) {
+      console.log(`You clicked on the pokemon ${this.pokemonList[index].name}`)
+    }
+  }
+
+  selectPokemon2(pokemonId : string) {
+    const pokemon : Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon) {
+      console.log(`You clicked on the pokemon ${pokemon.name}`)
+      this.pokemonSelected = pokemon
+    }
+    else {
+      console.log("Index out of range")
+      this.pokemonSelected = undefined
+    }
+  }
 }
