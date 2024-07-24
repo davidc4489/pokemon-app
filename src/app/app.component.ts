10e8c05c989a8a2bbcf024b71c91057476a3b858
davidc4489 <davidc4489@gmail.com>
2024-07-24 11:41:37 +0300

    scomponents separation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-import { BorderCardDirective } from './border-card.directive';
 import { CommonModule } from '@angular/common';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { RouterOutlet } from '@angular/router';
 
 @Component({              // Permet de construire un composant web
   selector: 'app-root',   // Permet de donner un nom au composant afin de l'identifier
   standalone: true,
-  // Permet de definir le contenu html du composant
-  templateUrl: 'app.component.html',
-  imports: [BorderCardDirective, CommonModule, PokemonTypeColorPipe],
+  templateUrl: 'app.component.html',  // Permet de definir le contenu html du composant
+  imports: [CommonModule, RouterOutlet],
   styles: []
 })
 
-export class AppComponent implements OnInit {
-  pokemonList : Pokemon[] = POKEMONS;
-  pokemonSelected : Pokemon | undefined ;
+export class AppComponent {}
 
-  ngOnInit(): void {
-    console.table(this.pokemonList) ;
-  }
 
-  selectPokemon(event : MouseEvent) {
-    const index : number = Number((event.target as HTMLInputElement).value) ;
-    if(index < 12 && index >= 0) {
-      console.log(`You clicked on the pokemon ${this.pokemonList[index].name}`)
-    }
-  }
 
-  selectPokemon2(pokemonId : string) {
-    const pokemon : Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if(pokemon) {
-      console.log(`You clicked on the pokemon ${pokemon.name}`)
-      this.pokemonSelected = pokemon
-    }
-    else {
-      console.log("Index out of range")
-      this.pokemonSelected = undefined
-    }
-  }
-}
+
+
+
+
+
+
+
+
+
+// Version Tutoriel
+
+// import { Component, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
+// import { POKEMONS } from './mock-pokemon-list';
+// import { Pokemon } from './pokemon';
+// import { BorderCardDirective } from './border-card.directive';
+// import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+
+// export class AppComponent implements OnInit {
+  // pokemonList : Pokemon[] = POKEMONS;
+  // pokemonSelected : Pokemon | undefined ;
+
+  // ngOnInit(): void {
+    // console.table(this.pokemonList) ;
+  // }
+
+  // selectPokemon(event : MouseEvent) {
+  //   const index : number = Number((event.target as HTMLInputElement).value) ;
+  //   if(index < 12 && index >= 0) {
+  //     console.log(`You clicked on the pokemon ${this.pokemonList[index].name}`)
+  //   }
+  // }
+
+  // selectPokemon2(pokemonId : string) {
+  //   const pokemon : Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+  //   if(pokemon) {
+  //     console.log(`You clicked on the pokemon ${pokemon.name}`)
+  //     this.pokemonSelected = pokemon
+  //   }
+  //   else {
+  //     console.log("Index out of range")
+  //     this.pokemonSelected = undefined
+  //   }
+  // }
+// }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
+import { CommonModule } from '@angular/common';
+import { BorderCardDirective } from '../border-card.directive';
 
 @Component({
   selector: 'app-list-pokemon',
   standalone: true,
-  imports: [],
+  imports: [PokemonTypeColorPipe, CommonModule, BorderCardDirective],
   templateUrl: './list-pokemon.component.html',
   styles: ``
 })
 export class ListPokemonComponent {
+
+  pokemonList : Pokemon[] = POKEMONS;
 
 }
