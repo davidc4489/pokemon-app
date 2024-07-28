f92a97321394ea359750b93d9a2f3a8c228ad1e5
davidc4489 <davidc4489@gmail.com>
2024-07-28 12:14:00 +0300

    detail pokemon

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
+import { CommonModule } from '@angular/common';
+import { BorderCardDirective } from '../border-card.directive';
 
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
-  imports: [],
-  templateUrl: './detail-pokemon.component.html',
-  styles: ``
+  imports: [PokemonTypeColorPipe, CommonModule, BorderCardDirective],
+  templateUrl: './detail-pokemon.component.html'
 })
 export class DetailPokemonComponent {
+  
+  pokemonList: Pokemon[];
+  pokemon: Pokemon | undefined;
+  
+  constructor(private route: ActivatedRoute) { }
 
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id') ;
+
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
+  }
 }
